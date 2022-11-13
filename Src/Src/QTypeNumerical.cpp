0652337a922f257ba57ee1e95000eb7e3b117cb1
Tret Elena <elena.tret@student.unitbv.ro>
2022-11-13 11:14:02 +0200

    am creat functiile pentru clasa QTypeNumerical

---
+++ Src/Src/QTypeNumerical.cpp
@@ -0,0 +1,28 @@
+#include "QTypeNumerical.h"
+
+QTypeNumerical::QTypeNumerical(const std::string& question, int answer)
+	:m_question{ question }, m_answer{ answer }
+{
+}
+
+void QTypeNumerical::setQuestion(const std::string& question)
+{
+	m_question = question;
+}
+
+void QTypeNumerical::setAnswer(const int& answer)
+{
+	m_answer = answer;
+}
+
+int QTypeNumerical::getAnswer() const
+{
+	return m_answer;
+}
+
+std::ostream& operator<<(std::ostream& out, const QTypeNumerical& q)
+{
+	out << q.m_question << std::endl;
+	out << q.m_answer << std::endl;
+	return out;
+}
