c02c22c41ac22c6b38cb56590b8de6509052a48c
AlexUnguranu <a.ungureanu0099@gmail.com>
2022-11-19 17:29:49 +0200

    am adaugat noi functii in clasa Board si am modificat  structura ei

--- Src/Src/Board.h
+++ Src/Src/Board.h
@@ -2,20 +2,26 @@
 #include "Zone.h"
 #include <optional>
 #include <vector>
-//TD :
+//TD :implementare dependente din clasa Zone
 class Board
 {
-	uint8_t m_NumberOfPlayers : 2; // Numarul maxim de jucatori este 4
-	uint8_t m_BoardWidth : 3;
-	uint8_t m_BoardHeight : 3;
+	uint8_t m_NumberOfPlayers ; // Numarul maxim de jucatori este 4
+	uint8_t m_BoardWidth ;
+	uint8_t m_BoardHeight ;
 	std::vector<std::optional<Zone>> m_board;
+	int m_totalScore;
 
 public:
 	Board(const uint8_t NumberOfPlayers = 2);
-
+	using Position = std::pair<int, int>;
+	friend std::ostream& operator <<(std::ostream& out, Board board);
+	const std::optional<Zone>& operator[](const Position& indices) const;
+	std::optional<Zone>& operator[](const Position& indices);
+	//
+	void ObtainTotalScore();
+	int getTotalScore();
 
 private:
 	void ChangeBoardDimensions();
-
 };
 
