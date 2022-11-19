0649910b76f2129e14e602882a9538a91eaa1f8b
Catalinul3 <constantin.zaharia@student.unitbv.ro>
2022-11-19 17:52:19 +0200

    Am actualizat membrii si functiile clasei Advantage Fifty-Fifty

--- Src/Src/Advantage_Fifty_Fifty.h
+++ Src/Src/Advantage_Fifty_Fifty.h
@@ -1,11 +1,16 @@
 #pragma once
+#include<string>
+#include<vector>
 class Advantage_Fifty_Fifty
 {private:
 	bool m_used = false;
-public:
-	Advantage_Fifty_Fifty();
+	std::string m_answer;
+	std::vector<std::string>m_variants;
+    public:
+	Advantage_Fifty_Fifty(std::string answer,std::vector<std::string>variants);
 	bool Advantage_used();
 	bool get_Advantage_Fifty_Fifty();
+
 
 };
 
--- Src/Src/Advantage_fifty_fifty.cpp
+++ Src/Src/Advantage_fifty_fifty.cpp
@@ -1,12 +1,25 @@
 
 #include "Advantage_Fifty_Fifty.h"
 
+
+
+Advantage_Fifty_Fifty::Advantage_Fifty_Fifty(std::string answer, std::vector<std::string> variants)
+{
+    this->m_answer = answer;
+    variants.resize(4);
+    for (int i=0;i<variants.size();i++)
+    {
+        this->m_variants[i] = variants[i];
+    }
+}
+
 bool Advantage_Fifty_Fifty::Advantage_used()
 {
-    
+    if (m_used == true)
+        return m_used;
 }
 
 bool Advantage_Fifty_Fifty::get_Advantage_Fifty_Fifty()
 {
-    
+    return m_used;
 }
