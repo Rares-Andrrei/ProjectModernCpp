ad5ef65bcf04acb1179c034e95fcef8d47e10df6
Tret Elena <elena.tret@student.unitbv.ro>
2022-11-20 12:40:43 +0200

    am creat clasa FourCloseAnswers care reprezinta un avantaj pentru intrebarile numerice

---
+++ Src/Src/FourCloseAnswers.h
@@ -0,0 +1,18 @@
+#pragma once
+#include <array>
+
+#include "QTypeNumerical.h"
+
+class FourCloseAnswers
+{
+private:
+	bool m_used = false;
+	std::array<int, 4> m_answers;
+
+public:
+	FourCloseAnswers() = default;
+	void GenerateVariants(QTypeNumerical q);
+	bool AdvantageIsUsed();
+	friend std::ostream& operator << (std::ostream& out, FourCloseAnswers const& answers);
+};
+
