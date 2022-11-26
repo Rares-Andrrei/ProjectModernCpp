b8d7ccf7ace7c62261a78953973073032645a7bc
AlexUnguranu <a.ungureanu0099@gmail.com>
2022-11-26 15:43:20 +0200

    Am Adaugat noi functionalitati clasei Zone
    Getter pentru score si operator de afisare

--- Src/Src/Zone.h
+++ Src/Src/Zone.h
@@ -7,15 +7,15 @@ class Zone
 protected:
 	uint16_t m_score;
 	bool m_captured;
-	Player* m_player;
+	Player m_player;
 public:
 	Zone() = default;
 	void SetScore(const uint16_t& score = 100);
-	void SetPlayer(Player* player);
+	void SetPlayer(Player player);
 	Zone operator +(const uint16_t& value);
 	friend Zone operator+(const uint16_t& value, Zone& zone);
 	void DecrementScore(); // folosit pentru clasa  AVANTAJE
+	friend std::ostream& operator<<(std::ostream& out, const Zone& zona);
+	uint16_t getScore();
 	~Zone() = default;
 };
-
-
