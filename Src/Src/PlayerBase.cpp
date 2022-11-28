83dd1ad9e6eae9633c6c239818e8881050fc329c
AlexUnguranu <a.ungureanu0099@gmail.com>
2022-11-28 15:51:37 +0200

    Am adaugat operatorul de afisare si de adunare pentru clasa PlayerBase
    Operatorul este asemanator cu cel din clasa Zone

--- Src/Src/PlayerBase.cpp
+++ Src/Src/PlayerBase.cpp
@@ -16,3 +16,24 @@ PlayerBase PlayerBase::operator--(int)
 {
 	return (*this)--;
 }
+
+PlayerBase PlayerBase::operator+(const uint16_t& value)
+{
+	this->m_score += value;
+	return *this;
+}
+
+PlayerBase operator+(const uint16_t& value, PlayerBase& zone)
+{
+	return  zone + value;
+}
+
+std::ostream& operator<<(std::ostream& out, const PlayerBase& zona)
+{
+	out <<"B:" << Player::ColorToString(zona.m_color) << ' ';
+	//out << "\nB:" << (const Zone&)zona;
+	
+
+	//out <<"B:"<< static_cast<const Zone&>(zona);
+	return out;
+}
--- Src/Src/PlayerBase.h
+++ Src/Src/PlayerBase.h
@@ -1,13 +1,19 @@
 #pragma once
 #include "Zone.h"
 
-class PlayerBase: public Zone
+class PlayerBase: virtual public Zone
 {
 	int m_lives = 3;
 public:
 	PlayerBase(Player::Color color);
 	PlayerBase operator--();
 	PlayerBase operator--(int);
+
+	PlayerBase operator +(const uint16_t& value);
+	friend PlayerBase operator+(const uint16_t& value, PlayerBase& zone);
+
+	friend std::ostream& operator<<(std::ostream& out, const PlayerBase& zona);
+
 	~PlayerBase()= default;
 };
 
