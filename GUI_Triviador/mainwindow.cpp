bfe7206496c0cd5c0c6e8656e436bc7006a2580b
Tret Elena <elena.tret@student.unitbv.ro>
2022-12-08 13:16:43 +0200

    am facut cateva verificari in clasa MainWindow

--- GUI_Triviador/mainwindow.cpp
+++ GUI_Triviador/mainwindow.cpp
@@ -1,15 +1,36 @@
 #include "mainwindow.h"
 #include "ui_mainwindow.h"
+#include <QPixmap>
 
 MainWindow::MainWindow(QWidget *parent)
     : QMainWindow(parent)
     , ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
+
+    logInWindow.reset(new LogIn());
+    signUpWindow.reset(new SignUp());
 }
 
 MainWindow::~MainWindow()
 {
     delete ui;
+}
+
+void MainWindow::on_pushButton_2_clicked()
+{
+    ui->stackedWidget->setCurrentIndex(1);
+}
+
+
+void MainWindow::on_pushButton_3_clicked()
+{
+    logInWindow->show();
+}
+
+
+void MainWindow::on_pushButton_4_clicked()
+{
+    signUpWindow->show();
 }
 
