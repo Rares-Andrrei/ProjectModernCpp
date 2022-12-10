4316fd66edb31f5ae266e751c11848bb27c37978
AlexUnguranu <a.ungureanu0099@gmail.com>
2022-12-10 21:55:30 +0200

    Am adaugat ruta pentru a te registra pe client

--- gui/signUp.cpp
+++ gui/signUp.cpp
@@ -1,4 +1,5 @@
 #include "signUp.h"
+#include <cpr/cpr.h>
 
 signUp::signUp(QWidget* parent)
     : QMainWindow(parent)
@@ -19,6 +20,26 @@ void signUp::onEnterButtonClicked()
 
     if (password != confirmPassword)
         QMessageBox::about(this, "Sign up error", "The passwords do not match");
-    else
-        QMessageBox::about(this, "Sign up", "The account is created successfully, now you can log in");
+    //else
+    //    QMessageBox::about(this, "Sign up", "The account is created successfully, now you can log in");
+	std::string usernameString, passwordString, nicknameString;
+	usernameString = username.toLocal8Bit().constData();
+	passwordString = password.toLocal8Bit().constData();
+	nicknameString = name.toLocal8Bit().constData();
+
+	auto response = cpr::Put(
+		cpr::Url{ "http://localhost:18080/register" },
+		cpr::Payload{
+			{ "username", usernameString },
+			{ "password", passwordString },
+			{ "nickname", nicknameString }
+		}
+	);
+
+	if (response.status_code == 200 ) {// register succesful
+		QMessageBox::information(this, "Success", "Account was registered");
+	}
+	else {
+		QMessageBox::information(this, "Failure", "The account wasn't registered");
+	}
 }
