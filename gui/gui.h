886cea2c1aa1759dddaff1b1f2b7d10ac54b01a3
Tret Elena <elena.tret@student.unitbv.ro>
2022-12-10 20:19:24 +0200

    am creat o clasa care contine fereastra de pornire a jocului

---
+++ gui/gui.h
@@ -0,0 +1,26 @@
+#pragma once
+
+#include <QtWidgets/QMainWindow>
+#include "ui_gui.h"
+
+#include "signUp.h"
+#include "logIn.h"
+
+class gui : public QMainWindow
+{
+    Q_OBJECT
+
+public:
+    gui(QWidget* parent = Q_NULLPTR);
+
+private:
+    Ui::guiClass ui;
+
+    std::shared_ptr<signUp> signUpWindow;
+    std::shared_ptr<logIn> logInWindow;
+
+private slots:
+    void onPlayButtonClicked();
+    void onSignUpButtonClicked();
+    void onLogInButtonClicked();
+};
