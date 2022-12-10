b5dbde78f52625cf7df8676b6301b4bac6b17a71
AlexUnguranu <a.ungureanu0099@gmail.com>
2022-12-10 22:38:35 +0200

    Am adaugat ruta pentru verificare username deja existent pe client si am facut verificare de  campuri goale

--- gui/signUp.cpp
+++ gui/signUp.cpp
@@ -2,32 +2,49 @@
 #include <cpr/cpr.h>
 
 signUp::signUp(QWidget* parent)
-    : QMainWindow(parent)
+	: QMainWindow(parent)
 {
-    ui.setupUi(this);
-    connect(ui.enterButton, SIGNAL(clicked()), SLOT(onEnterButtonClicked()));
+	ui.setupUi(this);
+	connect(ui.enterButton, SIGNAL(clicked()), SLOT(onEnterButtonClicked()));
 }
 
 void signUp::onEnterButtonClicked()
 {
-    QString name = ui.s_name->text();
-    QString username = ui.s_username->text();
-    QString password = ui.s_password->text();
-    QString confirmPassword = ui.s_confirmPassword->text();
+	QString name = ui.s_name->text();
+	QString username = ui.s_username->text();
+	QString password = ui.s_password->text();
+	QString confirmPassword = ui.s_confirmPassword->text();
 
-    //daca username ul nu e unic, eroare
-    //daca parola nu e la fel ca confimPassword, eroare
+	//daca username ul nu e unic, eroare
+	//daca parola nu e la fel ca confimPassword, eroare
 
-    if (password != confirmPassword)
-        QMessageBox::about(this, "Sign up error", "The passwords do not match");
-    //else
-    //    QMessageBox::about(this, "Sign up", "The account is created successfully, now you can log in");
+	if (password != confirmPassword)
+	{
+		QMessageBox::about(this, "Sign up error", "The passwords do not match");
+		return;
+	}
+
 	std::string usernameString, passwordString, nicknameString;
 	usernameString = username.toLocal8Bit().constData();
 	passwordString = password.toLocal8Bit().constData();
 	nicknameString = name.toLocal8Bit().constData();
-
+	if (usernameString.empty() || passwordString.empty() || nicknameString.empty())
+	{
+		QMessageBox::about(this, "Sign up error", "Please fill in all the fields");
+		return;
+	}
 	auto response = cpr::Put(
+		cpr::Url{ "http://localhost:18080/validateusername" },
+		cpr::Payload{
+			{"username", usernameString}
+		}
+	);
+	if (response.status_code == 400)
+	{
+		QMessageBox::information(this, "Username already used", "Selected Username isn't available");
+		return;
+	}
+	response = cpr::Put(
 		cpr::Url{ "http://localhost:18080/register" },
 		cpr::Payload{
 			{ "username", usernameString },
@@ -36,7 +53,8 @@ void signUp::onEnterButtonClicked()
 		}
 	);
 
-	if (response.status_code == 200 ) {// register succesful
+	if (response.status_code == 200) // register succesful
+	{
 		QMessageBox::information(this, "Success", "Account was registered");
 	}
 	else {
