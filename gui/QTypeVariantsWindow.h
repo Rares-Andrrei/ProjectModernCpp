d34819126a427e3eae0cd1dae02554f4420c7054
AlexUnguranu <a.ungureanu0099@gmail.com>
2023-01-02 18:11:48 +0200

    Am adaugat  butoanele si sloturile lor

---
+++ gui/QTypeVariantsWindow.h
@@ -0,0 +1,28 @@
+#pragma once
+
+#include <QMainWindow>
+#include "ui_QTypeVariantsWindow.h"
+#include <string>
+#include <QTimer>
+
+class QTypeVariantsWindow : public QMainWindow
+{
+	Q_OBJECT
+
+public:
+	QTypeVariantsWindow(QWidget* parent = nullptr);
+	~QTypeVariantsWindow();
+	void requestQuestion();
+
+private slots:
+	void on_Variant1_clicked();
+	void on_Variant2_clicked();
+	void on_Variant3_clicked();
+	void on_Variant4_clicked();
+	void on_Variant_clicked();
+	void on_Enter_clicked();
+
+	QPushButton* m_Variant = nullptr;
+	QString variant = "";
+	Ui::QTypeVariantsWindowClass ui;
+};
