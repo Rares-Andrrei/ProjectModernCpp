fa24509cfb9127cd5899bc596259a88bf8458c21
AlexUnguranu <a.ungureanu0099@gmail.com>
2023-01-02 18:15:42 +0200

    Am adaugat functia de request la intrebare

--- gui/QTypeVariantsWindow.cpp
+++ gui/QTypeVariantsWindow.cpp
@@ -13,8 +13,26 @@ QTypeVariantsWindow::QTypeVariantsWindow(QWidget* parent)
 	ui.VariantB->setSizePolicy(QSizePolicy::Expanding, QSizePolicy::Expanding);
 	ui.VariantC->setSizePolicy(QSizePolicy::Expanding, QSizePolicy::Expanding);
 	ui.VariantD->setSizePolicy(QSizePolicy::Expanding, QSizePolicy::Expanding);
+
+	requestQuestion();
 }
 
 QTypeVariantsWindow::~QTypeVariantsWindow()
 {
 }
+
+void QTypeVariantsWindow::requestQuestion()
+{
+	ui.Question->setWordWrap(true);
+
+	ui.Question->setFont(QFont("Arial", 25));
+	ui.Question->setAlignment(Qt::AlignCenter);
+	// Request :: ask the server for a random question and the 4 variants
+	ui.Question->setText("This is a long message that will be displayed in a scrollable text edit widget.This is a long message that will be displayed in a scrollable text edit widget.");
+
+	ui.VariantA->setText("A");
+	ui.VariantB->setText("B");
+	ui.VariantC->setText("C");
+	ui.VariantD->setText("D");
+}
+
