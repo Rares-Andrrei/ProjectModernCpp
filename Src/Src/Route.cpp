2b8b3bc153600b255f4a87f8e5bebb77afb1be6b
Rares <rares.andrei.tabacaru@gmail.com>
2023-01-02 22:33:59 +0200

    Am implementat ruta pentru lobby-ul de 2 playeri si cea de logOut in solutia serverului

--- Src/Src/Route.cpp
+++ Src/Src/Route.cpp
@@ -2,8 +2,9 @@
 #include "utils.h"
 #include<string>
 
-Route::Route(Database& db) : m_db{db}
+Route::Route(Database& db, std::shared_ptr<Players> players) : m_db{db}, m_players{players}
 {
+
 }
 
 void Route::loginRoute()
@@ -21,7 +22,7 @@ void Route::loginRoute()
 		std::string sessionKey;
 		if (check == CredentialErrors::Valid)
 		{
-			sessionKey = m_playerList.addActivePlayer(account);
+			sessionKey = m_players->addActivePlayer(account);
 		}
 		crow::response res;
 		res.body = std::to_string(static_cast<int>(check)) + sessionKey;
@@ -30,11 +31,59 @@ void Route::loginRoute()
 		});
 }
 
+void Route::enterTwoPlayersLobby()
+{
+
+	auto& queueTwoPlayers = CROW_ROUTE(m_app, "/queueTwoPlayerGame").methods(crow::HTTPMethod::Put);
+	queueTwoPlayers([this](const crow::request& req) {
+
+		auto bodyArgs = parseUrlArgs(req.body);
+	auto end = bodyArgs.end();
+	auto sessionKeyIter = bodyArgs.find("sessionKey");
+	if (m_players->isActive(sessionKeyIter->second))
+	{
+		std::string key = sessionKeyIter->second;
+		twoPlayers.push_back(m_players->getPlayer(key));
+	}
+	if (twoPlayers.size() < 2)
+	{
+		std::this_thread::sleep_for(std::chrono::milliseconds(100));
+		return crow::response(201); // 201 means that the player is queued
+	}
+	else
+	{
+		std::this_thread::sleep_for(std::chrono::milliseconds(100));
+		return crow::response(200); // the game stars when there are two players in the queue
+	}
+		});
+}
+
+void Route::exitTwoPlayersLobby()
+{
+	auto& exitQueue = CROW_ROUTE(m_app, "/eliminatePlayerFromQueue").methods(crow::HTTPMethod::PUT);
+	exitQueue([this](const crow::request& req) {
+		auto bodyArgs = parseUrlArgs(req.body);
+	auto end = bodyArgs.end();
+	auto sessionKeyIter = bodyArgs.find("sessionKey");
+	std::string playerName = m_players->getPlayer(sessionKeyIter->second)->getName();
+	for (auto& it : twoPlayers)
+	{
+		if (it->getName() == playerName)
+		{
+			twoPlayers.remove(it);
+			return crow::response(200);
+		}
+	}
+	return crow::response(200);
+		});
+}
+
+
 void Route::signUpRoute()
 {
-	auto& login = CROW_ROUTE(m_app, "/signUp")
+	auto& signUp = CROW_ROUTE(m_app, "/signUp")
 		.methods(crow::HTTPMethod::Post);
-	login([this](const crow::request& req) {
+	signUp([this](const crow::request& req) {
 		auto bodyArgs = parseUrlArgs(req.body);
 	auto usernameIter = bodyArgs.find("username");
 	auto passwordIter = bodyArgs.find("password");
@@ -43,6 +92,20 @@ void Route::signUpRoute()
 	CredentialErrors check = m_db.registerUser(account);
 	crow::response res;
 	res.body = std::to_string(static_cast<int>(check));
+	res.code = 200;
+	return res;
+		});
+}
+
+void Route::logOutRoute()
+{
+	auto& logOut = CROW_ROUTE(m_app, "/logOut")
+		.methods(crow::HTTPMethod::Put);
+	logOut([this](const crow::request& req) {
+		auto bodyArgs = parseUrlArgs(req.body);
+	auto sessionKey = bodyArgs.find("sessionKey");
+	m_players->logOutPlayer(sessionKey->second);
+	crow::response res;
 	res.code = 200;
 	return res;
 		});
