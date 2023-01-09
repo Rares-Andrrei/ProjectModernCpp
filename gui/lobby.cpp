a8b3fbb0bb6927cf3ec3b970714063a501bad903
Tret Elena <elena.tret@student.unitbv.ro>
2023-01-09 23:35:45 +0200

    am adaugat pozele impreuna cu butoanele de next si previous

--- gui/lobby.cpp
+++ gui/lobby.cpp
@@ -1,11 +1,7 @@
 #include "lobby.h"
-#include <QMovie>
 
-lobby::lobby(Route& route, QWidget* parent)
-	: m_routes(route), QMainWindow(parent)
+void lobby::stylingTheLobby()
 {
-	ui.setupUi(this);
-
 	ui.twoPlayersButton->setStyleSheet("QPushButton { background-image:url(:/gui/twoPlayers.png); text-align: left, up; border-radius: 5px; }");
 	ui.threePlayersButton->setStyleSheet("QPushButton { background-image:url(:/gui/threePlayers.png); text-align: left, up; border-radius: 5px; }");
 	ui.fourPlayersButton->setStyleSheet("QPushButton { background-image:url(:/gui/fourPlayers.png); text-align: left, up; border-radius: 5px; }");
@@ -14,11 +10,62 @@ lobby::lobby(Route& route, QWidget* parent)
 	ui.loadingLabel->setMovie(movie);
 	ui.loadingLabel->show();
 	movie->start();
+}
+
+void lobby::buttonsConnections()
+{
 	connect(ui.twoPlayersButton, SIGNAL(clicked()), SLOT(onTwoPlayersButtonClicked()));
 	connect(ui.threePlayersButton, SIGNAL(clicked()), SLOT(onThreePlayersButtonClicked()));
 	connect(ui.fourPlayersButton, SIGNAL(clicked()), SLOT(onFourPlayersButtonClicked()));
 
 	connect(ui.cancelButton, SIGNAL(clicked()), SLOT(onCancelButtonClicked()));
+}
+
+void lobby::catPictures()
+{
+	filenames.append(":/slideShowCats/cat1.png");
+	filenames.append(":/slideShowCats/cat2.png");
+	filenames.append(":/slideShowCats/cat3.png");
+	filenames.append(":/slideShowCats/cat4.png");
+	filenames.append(":/slideShowCats/cat5.png");
+	filenames.append(":/slideShowCats/cat6.png");
+	filenames.append(":/slideShowCats/cat7.png");
+	filenames.append(":/slideShowCats/cat8.png");
+	ui.catPicturesLabel->setPixmap(filenames.at(i));
+}
+
+void lobby::nextOrPrevious()
+{
+	int maxSize = filenames.size() - 1;
+	if (i > maxSize) { i = 0; }
+	if (i < 0) { i = maxSize; }
+
+	if (i <= maxSize && i >= 0) {
+		ui.catPicturesLabel->setPixmap(filenames.at(i));
+	}
+}
+
+
+lobby::lobby(Route& route, QWidget* parent)
+	: m_routes(route), QMainWindow(parent)
+{
+	ui.setupUi(this);
+
+	stylingTheLobby();
+	buttonsConnections();
+	catPictures();
+}
+
+void lobby::on_nextButton_clicked()
+{
+	i++;
+	nextOrPrevious();
+}
+
+void lobby::on_previousButton_clicked()
+{
+	i--;
+	nextOrPrevious();
 }
 
 void lobby::setPlayer(const QString& playerName)
@@ -38,10 +85,12 @@ lobby::~lobby()
 		QMessageBox::information(this, "Failure", "The logout failed");
 	}
 }
+
 void lobby::onTwoPlayersButtonClicked()
 {
 	ui.lobbyGameModes->setCurrentIndex(1);
 	m_routes.enterLobby(2, m_players);
+
 	for (auto& player : m_players)
 	{
 		if (player->getName() == m_PlayerName)
@@ -50,6 +99,7 @@ void lobby::onTwoPlayersButtonClicked()
 			break;
 		}
 	}
+
 	if (m_players.size() > 0)
 	{
 		Game.reset(new TriviadorGame());
@@ -103,3 +153,4 @@ void lobby::onCancelButtonClicked()
 		QMessageBox::information(this, "queue", "Error leaving the queue");
 	}
 }
+
