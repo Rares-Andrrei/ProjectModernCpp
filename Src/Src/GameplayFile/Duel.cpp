e0a007f5d746a5ceb1466b2a7465cadec89a4747
Rares <rares.andrei.tabacaru@gmail.com>
2023-01-15 02:53:53 +0200

    Am modificat clasa duel

--- Src/Src/GameplayFile/Duel.cpp
+++ Src/Src/GameplayFile/Duel.cpp
@@ -1,135 +1,168 @@
 #include "Duel.h"
 
-Duel::Duel(const Color::ColorEnum& player, std::shared_ptr<Zone> zone)
-	:m_duelingPlayers{ player, zone->getColor() }, m_zone{ zone }, m_draw{ false }
+Duel::Duel(const Color::ColorEnum& player, std::shared_ptr<Zone> zone, int zoneId, std::shared_ptr<Database> db)
+	:m_duelingPlayers{ player, zone->getColor() }, m_zone{ zone }, m_draw{ false }, m_zoneId{ zoneId }, m_db{ db }, m_numerical{NumericQuestionManager(2)}
 {
+	generateQuestion();
 }
 
-
-void Duel::generateQuestion(QuestionManager& questions)
+void Duel::getDuelInfo(Color::ColorEnum& c1, Color::ColorEnum& c2, int& zoneId)
 {
-	m_qTypeVariants = questions.randQTypeVariants();
-	m_qTypeNumerical = questions.randQTypeNumerical();
+	c1 = m_duelingPlayers.first;
+	c2 = m_duelingPlayers.second;
+	zoneId = m_zoneId;
 }
 
-void Duel::startDuel()
+void Duel::setDuelVaraintsAnswer(Color::ColorEnum color, int variant)
 {
-	std::string attackerResponse;
-	std::string defenderResonse;
-	std::cout << m_qTypeVariants.value();
-	std::string aux;
-	std::getline(std::cin,  aux);
-	std::cout << Color::ColorToString(m_duelingPlayers.first) << " player answer:";
-	std::getline(std::cin, attackerResponse);
-	std::cout << Color::ColorToString(m_duelingPlayers.second) << " player answer:";
-	std::getline(std::cin, defenderResonse);
-
-	this->giveAnswers(attackerResponse, defenderResonse);
-
-	if (m_winner == Color::ColorEnum::None && m_draw)
-	{
-		std::cout << m_qTypeNumerical.value();
-		int attackerNumericalResponse, defenderNumericalResponse;
-		std::cin >> attackerNumericalResponse >> defenderNumericalResponse;
-
-		this->giveNumericalAnswers({ attackerNumericalResponse,0,m_duelingPlayers.first }, { defenderNumericalResponse,0,m_duelingPlayers.second });
-	}
-	std::cout << "The Winner is PlayerID:  " << int(m_winner) << " or Color: " << Color::ColorToString(m_winner) << std::endl;
-	rewardWinner();
-	std::cout << std::endl;
+	m_responses[color] = variant;
 }
 
-
-void Duel::giveAnswers(int attackerAnswer, int defenderAnswer)
+void Duel::setNumericalAnswer(int time, int response, const Color::ColorEnum& color)
 {
-	auto& [attacker, defender] = m_duelingPlayers;
-	const int corectAnswer = m_qTypeNumerical.value().getAnswer();
-	if (attackerAnswer == defenderAnswer == corectAnswer)
-	{
-		return;
-	}
-	else if (attackerAnswer == corectAnswer)
-	{
-		m_winner = attacker;
-	}
-	else if (defenderAnswer == corectAnswer)
-	{
-		m_winner = defender;
-	}
-
+	m_numerical.addPlayerResponse(time, response, color);
 }
 
-void Duel::giveAnswers(std::string attackerAnswer, std::string defenderAnswer)
+QTypeVariants Duel::getQTypeVariants()
 {
-	auto& [attacker, defender] = m_duelingPlayers;
-	std::string corectAnswer = m_qTypeVariants.value().getAnswer();
-	if (attackerAnswer == defenderAnswer)
-	{
-		if (attackerAnswer == corectAnswer)
-		{
-			m_draw = true;
-			return;
-		}
-	}
-	else if (attackerAnswer == corectAnswer)
-	{
-		m_winner = attacker;
-	}
+	return m_qTypeVariants.value();
 }
 
-void Duel::giveNumericalAnswers(std::tuple<int, int, Color::ColorEnum> attacker, std::tuple<int, int, Color::ColorEnum> defender)
+QTypeNumerical Duel::getQTypeNumerical()
 {
-	m_draw = false;
-	auto& [attackerAnswer, attackerResponseTime, attackerPlayer] = attacker;
-	auto& [defenderAnswer, defenderResponseTime, defenderPlayer] = defender;
-	int corectAnswer = m_qTypeNumerical.value().getAnswer();
-
-	int attackerAnswerError = abs(attackerAnswer - corectAnswer);
-	int defenderAnswerError = abs(defenderAnswer - corectAnswer);
-
-	if (attackerAnswerError == defenderAnswerError)
-	{
-		if (attackerResponseTime == defenderResponseTime)
-		{
-			m_draw = true;
-			return;
-		}
-		else if (attackerResponseTime < defenderResponseTime)
-		{
-			m_winner = attackerPlayer;
-		}
-	}
-	else if (attackerAnswerError < defenderAnswerError)
-	{
-		m_winner = attackerPlayer;
-	}
+	return m_numerical.getQuestion();
 }
 
-void Duel::rewardWinner()
+bool Duel::isDraw()
 {
-	auto& [attacker, defender] = m_duelingPlayers;
+	return m_draw;
+}
 
+Color::ColorEnum Duel::getAttacker()
+{
+	return m_duelingPlayers.first;
+}
 
-	if (m_winner == defender)
+void Duel::setWinner(Board& board)
+{
+	std::vector<Color::ColorEnum> m_winners;
+	if (m_draw)
 	{
-		*m_zone = *m_zone + 100;
+		m_winners.push_back(m_numerical.getPlayersOrder()[0]);
 	}
 	else
 	{
-		auto isBase = std::dynamic_pointer_cast<PlayerBase>(m_zone);
-		if (std::dynamic_pointer_cast<PlayerBase>(m_zone) != nullptr)
+		for (auto resp : m_responses)
 		{
-			isBase->DecrementLives();
+			if (resp.second == m_qTypeVariants->getAnswerIndex())
+			{
+				m_winners.push_back(resp.first);
+			}
 		}
-		else if (m_zone->getScore() == 100)
+	}
+	if (m_winners.size() == 0)
+	{
+		m_state = DuelState::Lose;
+		return;
+	}
+	else if (m_winners.size() == 1)
+	{
+		auto& [attacker, defender] = m_duelingPlayers;
+		auto isBase = std::dynamic_pointer_cast<PlayerBase>(m_zone);
+		if (m_winners[0] == defender && std::dynamic_pointer_cast<PlayerBase>(m_zone) == nullptr)
 		{
-			m_zone->changeOwner(attacker);
+			*m_zone = *m_zone + 100;
+			m_updatedZones.push_back(m_zone);
+			m_state = DuelState::Win;
+			return;
+		}
+		else if (m_winners[0] == defender && std::dynamic_pointer_cast<PlayerBase>(m_zone) != nullptr)
+		{
+			m_state = DuelState::Lose;
+			return;
 		}
 		else
 		{
-			m_zone->DecrementScore();
+			if (std::dynamic_pointer_cast<PlayerBase>(m_zone) != nullptr)
+			{
+				isBase->DecrementLives();
+				if (isBase->getNumberOfLifesLeft() == 0)
+				{
+					Color::ColorEnum conquerdPlayer = isBase->getColor();
+					int zoneId = 0;
+					while (board[zoneId] != board.end())
+					{
+						if (board[zoneId] == isBase)
+						{
+							board[zoneId].reset();
+							board[zoneId] = std::make_shared<Zone>(attacker);
+							m_updatedZones.push_back(board[zoneId]);
+						}
+						else if (board[zoneId]->getColor() == attacker)
+						{
+							board[zoneId]->changeOwner(attacker);
+							m_updatedZones.push_back(board[zoneId]);
+						}
+						zoneId++;
+					}
+					m_state = DuelState::Win;
+					return;
+				}
+				else
+				{
+					m_updatedZones.push_back(m_zone);
+					m_state = DuelState::lifeTaken;
+					return;
+				}
+
+			}
+			else if (m_zone->getScore() == 100)
+			{
+				m_zone->changeOwner(attacker);
+				m_updatedZones.push_back(m_zone);
+				m_state = DuelState::Win;
+				return;
+			}
+			else
+			{
+				m_zone->DecrementScore();
+				m_updatedZones.push_back(m_zone);
+				m_state = DuelState::Win;
+				return;
+			}
 		}
 	}
+	else
+	{
+		m_draw = true;
+		m_state = DuelState::Draw;
+		return;
+	}
 
+}
+
+Duel::DuelState Duel::getState()
+{
+	if (m_state.has_value())
+	{
+		return m_state.value();
+	}
+	return DuelState::None;
+}
+
+int Duel::getZoneId()
+{
+	return m_zoneId;
+}
+
+std::vector<std::shared_ptr<Zone>> Duel::getUpdatedZones()
+{
+	return m_updatedZones;
+}
+
+void Duel::generateQuestion()
+{
+	m_qTypeVariants = m_db->randQTypeVariants();
+	m_numerical.setQuestion(m_db->randQTypeNumerical());
 }
 
--- Src/Src/GameplayFile/Duel.h
+++ Src/Src/GameplayFile/Duel.h
@@ -3,27 +3,50 @@
 #include <optional>
 #include "../../Player/Player/Player.h"
 #include <utility>
+#include "Board.h"
+#include "NumericQuestionManager.h"
 #include <PlayerBase.h>
 
 class Duel
 {
-	static const uint8_t time = 30;
+public:
+	enum class DuelState{
+		Win,
+		Lose,
+		Draw,
+		lifeTaken,
+		None
+	};
+private:
+	std::shared_ptr<Database> m_db;
+
+	std::optional<Duel::DuelState> m_state;
 	std::pair<Color::ColorEnum, Color::ColorEnum> m_duelingPlayers;
+	std::unordered_map<Color::ColorEnum, int> m_responses;
+
+	int m_zoneId;
 	std::shared_ptr<Zone> m_zone;
+
 	std::optional<QTypeVariants> m_qTypeVariants;
-	std::optional<QTypeNumerical> m_qTypeNumerical;
+
+	NumericQuestionManager m_numerical;
+
+	std::vector<std::shared_ptr<Zone>> m_updatedZones;
 	Color::ColorEnum m_winner;
 	bool m_draw;
 
+	void generateQuestion();
 public:
-	Duel(const Color::ColorEnum& player, std::shared_ptr<Zone> zone);
-	void generateQuestion(QuestionManager& questions);
-	void startDuel();
-
-private:
-	void giveAnswers(int attacker, int defender);
-	void giveAnswers(std::string attacker, std::string defender);
-
-	void giveNumericalAnswers(std::tuple<int, int, Color::ColorEnum> attacker, std::tuple<int, int, Color::ColorEnum> defender);
-	void rewardWinner();
+	Duel(const Color::ColorEnum& player, std::shared_ptr<Zone> zone, int zoneId, std::shared_ptr<Database> db);
+	void getDuelInfo(Color::ColorEnum& c1, Color::ColorEnum& c2, int& zoneId);
+	void setDuelVaraintsAnswer(Color::ColorEnum color, int variant);
+	void setNumericalAnswer(int time, int response, const Color::ColorEnum& color);
+	QTypeVariants getQTypeVariants();
+	QTypeNumerical getQTypeNumerical();
+	bool isDraw();
+	Color::ColorEnum getAttacker();
+	void setWinner(Board& board);
+	DuelState getState();
+	int getZoneId();
+	std::vector<std::shared_ptr<Zone>> getUpdatedZones();
 };
